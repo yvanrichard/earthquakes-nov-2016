919188d4404556366180aca3634bb38a75791c0e
yvan <yvan@dragonfly.co.nz>
2016-11-14 22:28:47 +1300

    Clean-up

--- ui.r
+++ ui.r
@@ -67,10 +67,8 @@ function(request) {
                             column(width = 6,
                                    box(width = '100%',
                                        title = 'Interactive 3D plot of quakes (Note: depth is not to scale)',
-                                       ## footer = 'Depth is not to scale',
                                        rglwidgetOutput('scatter3d', width = '100%', height=700))
                                        ## playwidgetOutput('scatter3dp', width = '100%', height=700))
-                                       ## scatterplotThreeOutput('scatter3d', width = '100%', height=700))
                                    )),
                    fluidRow(column(width = 6,
                                    box(width = '100%',
@@ -89,76 +87,3 @@ function(request) {
                )
     )
 }
-
-    ##                tabsetPanel(
-    ##                    id="tabvals",
-    ##                    tabPanel(
-    ##                        "Map",
-    ##                        fluidRow(
-    ##                            column(
-    ##                                width = 6,
-    ##                                align = 'center',
-    ##                                box(width = '100%',
-    ##                                    textOutput('xyplottxt'),
-    ##                                    plotOutput("xyplot", width = '100%', height = '500px')
-    ##                                    )
-    ##                            ),
-    ##                            column(
-    ##                                3,
-    ##                                align = 'center',
-    ##                                box(width = '100%',
-    ##                                    textOutput('yvartxt'),
-    ##                                    imageOutput("ymap", width = '100%', height = 'auto'),
-    ##                                    hr(),
-    ##                                    plotOutput("ydensplot", width = '100%', height = '150px')
-    ##                                    )
-    ##                            ),
-    ##                            column(
-    ##                                3,
-    ##                                align = 'center',
-    ##                                box(width = '100%',
-    ##                                    textOutput('xvartxt'),
-    ##                                    imageOutput("xmap", width = '100%', height = 'auto'),
-    ##                                    hr(),
-    ##                                    plotOutput("xdensplot", width = '100%', height = '150px')
-    ##                                    )
-    ##                            )),
-    ##                        fluidRow(
-    ##                            column(
-    ##                                offset=9,
-    ##                                width=3,
-    ##                                HTML('<div align="right"><a href="http://www.dragonfly.co.nz" align="right"><img src="data/logo.png" width="250px"></a></div>'))
-    ##                        )
-    ##                    ),
-    ##                    tabPanel(
-    ##                        "Planning units",
-    ##                        leafletOutput('plunmap2', width='100%', height=650)
-    ##                    ),
-    ##                    tabPanel(
-    ##                        "Index creator",
-    ##                        fluidRow(
-    ##                            column(width = 6,
-    ##                                   box(width = 12,
-    ##                                       title = 'Decision tree',
-    ##                                       helpText('Click anywhere to add a variable and the associated decision rule. Add more variables by clicking on the centre of the desired node.'),
-    ##                                       plotOutput('treeplot', height = 500,
-    ##                                                  click = "treeplot_click",
-    ##                                                  brush = brushOpts(id = "treeplot_brush")),
-    ##                                       actionButton("cleartree", "Clear tree"))),
-    ##                            column(width = 6,
-    ##                                   box(width = 12,
-    ##                                       title = 'Resulting categories',
-    ##                                       helpText("Specify the names of the categories from the decision tree once you're done, then click the button to create the variable.  The newly created variable will appear at the bottom of the variable list in the selectors of the left panels"),
-    ##                                       rHandsontableOutput("groupstable"),
-    ##                                       br(),
-    ##                                       actionButton("createvar", "Create/Update variable"),
-    ##                                       hr(),
-    ##                                       helpText('Distribution of the created variable among reaches'),
-    ##                                       rHandsontableOutput('createdsummary')))
-    ##                        ),
-    ##                        verbatimTextOutput('treelog'),
-    ##                        verbatimTextOutput('rndlog')
-    ##                    )
-    ##                )
-    ##            )
-    ## )
